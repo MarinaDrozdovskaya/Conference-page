f4a39c7cacbaf115354430558fcb803ae7d2d14b
Marina Drozdovskaya <85951373+MarinaDrozdovskaya@users.noreply.github.com>
2021-08-02 20:58:38 +0300

    .

--- javascript.js
+++ javascript.js
@@ -21,3 +21,10 @@ window.onload = () => {
         appearOnScroll.observe(fader);
     });
 }
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbar-list')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
